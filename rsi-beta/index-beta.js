32fc1d3dc6904e6431cca30eb0ce477727373cec
James Flanagan <jflanagan622@gmail.com>
2017-04-01 15:10:53 -0400

    Update Beta

---
+++ rsi-beta/index-beta.js
@@ -0,0 +1,142 @@
+// Richwood Scientific Bootcamp
+// Basic Node+Express server for
+// our test company
+var express = require('express');
+var app = express();
+
+// Set the port to listen on. 3000 in this example
+app.set('port', process.env.PORT || 3000);
+
+// Setup to serve static files
+app.use(express.static(__dirname + '/public'));
+
+// Add BodyParser to read HTTP message body
+var bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// Morgan for logging
+var morgan = require('morgan');
+app.use(morgan(':date :remote-addr :method :url :status :response-time ms - :res[content-length]'));
+
+// Add / Setup handlebars view engine
+var handlebars = require('express-handlebars');
+// Point to a default template
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+
+// Add handlebars to the app
+app.set('view engine', 'handlebars');
+
+// Kill cache 304 response
+app.disable('etag');
+
+
+// =============================================================================
+// Postgres
+// =============================================================================
+var config = {
+  user: 'postgres', 				// env var: PGUSER
+  database: 'postgres', 	// env var: PGDATABASE
+  password: 'darkride7', 			// env var: PGPASSWORD
+  host: 'localhost', 				// Server hosting the postgres database
+  port: 5432, 						// env var: PGPORT ** CHECK YOUR PORT
+  max: 10, 							// max number of clients in the pool
+  idleTimeoutMillis: 30000	 		// how long a client is allowed to remain idle before being closed
+};
+var Pool = require('pg-pool')
+global.pool = new Pool(config)
+
+// attach an error handler to the pool for when a connected, idle client
+// receives an error by being disconnected, etc
+pool.on('error', function(error, client) {
+  // handle this in the same way you would treat process.on('uncaughtException')
+  // it is supplied the error as well as the idle client which received the error
+  console.log('Pool received an error: ' + error)
+});
+
+
+//-----------------------------------------
+// Startup the server
+app.listen(app.get('port'), function(){
+	console.log( 'The Server is running. Open a browser and navigate to: http://localhost:3000');
+});
+
+
+
+
+//-----------------------------------------
+// Setup the routes
+//
+
+
+//-----------------------------------------
+// Page routes
+//
+
+// Default page
+app.get('/construction', function(req,res) {
+	// Send the construction page
+	res.render('construction');
+});
+
+app.get('/', function(req,res) {
+	// Send the construction page
+	res.render('home');
+});
+
+// Stub of login page
+app.get("/login", function (req, res) {
+	// Send the login page
+	res.render('login');
+});
+
+// Request to actually login
+app.post("/login", function (req, res) {
+    console.log(req.body.first_name);
+	// Close connection
+	res.status(200).json({result: 'success', data:{}});
+});
+
+// Page to add products to the Database
+app.get("/addproduct", function (req, res) {
+	// Send the Add Product page
+    res.render('addproduct-beta');
+});
+
+
+//-----------------------------------------
+// API routes
+//
+
+//-----------------------------------------
+// Products
+
+var products = require('./routes/products');
+
+// Create
+app.post('/api/product', products.createProduct);
+
+// Read all
+app.get('/api/products', products.readProducts);
+
+// Read one
+app.get('/api/product/:id', products.readProduct);
+
+// Update
+app.put('/api/product', products.updateProduct);
+
+// Delete
+app.delete('/api/product', products.deleteProduct);
+
+//-----------------------------------------
+
+
+// Finally If no routes match, send the 404 page
+app.use(function(req,res) {
+	res.status(404);
+	// Send 404 status code
+	res.render('404');
+});
+
---
+++ rsi-beta/routes/products.js
@@ -0,0 +1,187 @@
+"use strict";
+
+// UUID
+var uuid = require('node-uuid');
+
+// Dates
+var dateutil = require('dateutil');
+
+// Stream
+var QueryStream = require('pg-query-stream');
+
+
+//-------------------------------------------------------------------------------------------
+// Create a Product
+exports.createProduct = function(req, res) {
+	
+	pool.connect(function(err, client, done) {
+		
+		var handleError = function(err) {
+			// no error occurred, continue with the request
+			if(!err) return false;
+			res.status(500).json({ error: err });
+			return true;
+	    };
+	    // handle an error from the connect
+		if(handleError(err)) return;
+		
+		// Validate then insert
+		if(req.body.product_name) {
+			
+			var queryText = 'INSERT INTO products (id, product_name, unit_price, cost) VALUES ($1, $2, $3, $4) RETURNING id'
+			client.query(queryText, [uuid.v4(), req.body.product_name, req.body.unit_price, req.body.cost ], function(err, result) {
+				done();
+				// handle an error from the query
+				if(handleError(err)) return;
+				// Either
+				res.status(200).json({result: 'success', data:{ id : result.rows[0].id }});	
+				// Or sample redirect
+				//res.writeHead(302, {'Location': 'http://localhost:3000/addproduct'});
+				//res.end();
+			});
+	  	
+		} else {
+			done();
+	    	res.status(400).json({ error: 'product_name is required' });
+		}
+		
+	});
+	
+}
+
+
+//-------------------------------------------------------------------------------------------
+// Get all Products as a Stream
+exports.readProducts = function(req, res) {
+	
+	// get a pg client from the connection pool
+	pool.connect(function(err, client, done) {
+		
+    	var handleError = function(err) {
+			// no error occurred, continue with the request
+			if(!err) return false;
+			done();
+			console.log(err);
+			res.status(500).json({ error: err });
+			return true;
+    	};
+    	
+    	// handle an error from the connect
+		if(handleError(err)) return;
+		var queryText = 'SELECT * FROM products;';
+		client.query(queryText, [], function(err, result) {
+			if(handleError(err)) return;
+			done();
+			if(result.rowCount > 0) {
+				var products = result.rows;
+				res.status(200).json({result: 'success', data:{ products : products }});
+			} else {
+				res.status(200).json({result: 'success', data:{}});
+			}
+		});
+	});
+};
+
+
+
+//-------------------------------------------------------------------------------------------
+// Get specific Product
+exports.readProduct = function(req, res) {
+	
+	// get a pg client from the connection pool
+	pool.connect(function(err, client, done) {
+		
+    	var handleError = function(err) {
+			// no error occurred, continue with the request
+			if(!err) return false;
+			done(client);
+			res.status(500).json({ error: err });
+			return true;
+    	};
+    	
+    	// handle an error from the connect
+		if(handleError(err)) return;
+
+		var queryText = 'SELECT * FROM products WHERE id = $1;';
+		client.query(queryText, [req.params.id], function(err, result) {
+			if(handleError(err)) return;
+			done();
+			if(result.rowCount > 0) {
+				var products = result.rows;
+				res.status(200).json({result: 'success', data:{ products : products }});
+			} else {
+				res.status(200).json({result: 'success', data:{}});
+			}
+		}); 
+
+	});
+};
+
+//---------------------------------------------------------------------------------------
+// Update
+exports.updateProduct = function(req, res) {
+	
+	// get a pg client from the connection pool
+	pool.connect(function(err, client, done) {
+
+    	var handleError = function(err) {
+			// no error occurred, continue with the request
+			if(!err) return false;
+			done();
+			res.status(500).json({ result:'error', data:{ error:err } });
+			return true;
+    	};
+
+    	// handle an error from the connect
+		if(handleError(err)) return;
+
+		// Validate then insert
+		if(req.params.id) {
+			done();
+			res.status(200).json({result: 'success', data:{count : result.rowCount}});
+    	} else {
+	    	done();
+	    	res.status(400).json({ error: 'id is required' });
+    	}
+   
+  	});
+};
+
+//---------------------------------------------------------------------------------------
+// Delete
+exports.deleteProduct = function(req, res) {
+	
+	// get a pg client from the connection pool
+	pool.connect(function(err, client, done) {
+
+    	var handleError = function(err) {
+			// no error occurred, continue with the request
+			if(!err) return false;
+			done();
+			res.status(500).json({ result:'error', data:{ error:err } });
+			return true;
+    	};
+
+    	// handle an error from the connect
+		if(handleError(err)) return;
+
+		// Validate then insert
+		if(req.params.id) {
+			
+			var queryText = 'DELETE FROM products WHERE id = $1'
+			client.query(queryText, [req.params.id], function(err, result) {
+    			// handle an error from the query
+				if(handleError(err)) return;
+				done();
+				res.status(200).json({result: 'success', data:{count : result.rowCount}});
+      		});
+      	
+    	} else {
+	    	done();
+	    	res.status(400).json({ error: 'id is required' });
+    	}
+   
+  	});
+};
+
+
